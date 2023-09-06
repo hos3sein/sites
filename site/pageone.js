4a1a606e6a50e2a95f8540d795f47cecdae8c261
hos3sein <143866200+hos3sein@users.noreply.github.com>
2023-09-06 10:39:46 +0330

    Add files via upload

---
+++ site/pageone.js
@@ -0,0 +1,57 @@
+
+
+// 	function func1() {
+// 		document.getElementById('pic1').style.width = '500px';
+// 		document.getElementById('pic1').style.height = '500px';
+// 	}
+
+// 	function func2() {
+// 		document.getElementById('pic1').style.width = '300px';
+// 		document.getElementById('pic1').style.height = '300px';
+// 	}
+
+// 	function func3() {
+// 	document.getElementById('insta').style.width = '200px';
+// 	document.getElementById('insta').style.height = '200px';
+// }
+
+// 	function func4() {
+// 	document.getElementById('insta').style.width = '150px';
+// 	document.getElementById('insta').style.height = '150px';
+// }
+
+// function func5() {
+// 	document.getElementById('telegram').style.width = '200px';
+// 	document.getElementById('telegram').style.height = '200px';
+// }
+
+// 	function func6() {
+// 	document.getElementById('telegram').style.width = '150px';
+// 	document.getElementById('telegram').style.height = '150px';
+// }
+
+
+// function func7() {
+// 	document.getElementById('twitter').style.width = '200px';
+// 	document.getElementById('twitter').style.height = '200px';
+// }
+
+// 	function func8() {
+// 	document.getElementById('twitter').style.width = '150px';
+// 	document.getElementById('twitter').style.height = '150px';
+// }
+
+
+// 	document.getElementById('pic1').addEventListener("mouseover", func1);
+// 	document.getElementById("pic1").addEventListener("mouseout", func2);
+// 	document.getElementById('insta').addEventListener("mouseover", func3);
+// 	document.getElementById('insta').addEventListener("mouseout", func4);
+// 	document.getElementById('telegram').addEventListener("mouseover", func5);
+// 	document.getElementById('telegram').addEventListener("mouseout", func6);
+// 	document.getElementById('twitter').addEventListener("mouseover", func7);
+// 	document.getElementById('twitter').addEventListener("mouseout", func8);
+
+
+
+
+document.getElementById('name').style.color = 'red';
